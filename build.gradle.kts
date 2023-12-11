d7f26b103bca90deb84fdb28122b25c0bd3854fd
JDJG Inc. Official <ghostzap123@gmail.com>
2023-12-11 13:52:07 -0500

    Starting stuff for this.

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+repositories {
+    maven { url = "https://api.modrinth.com/maven" }
+}
+
+dependencies { implementation fg . deobf ("maven.modrinth:dalek-mod:67.0.0") }
+
+repositories {
+    maven { url = "https://cursemaven.com/" }
+}
+
+dependencies {
+    implementation fg.deobf("curse.maven:the-dalek-mod-59654:4706185") // 68.1.1
+}
